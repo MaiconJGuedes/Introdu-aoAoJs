984a0697fab39314c398681e058427c090539d11
MaiconJGuedes <maicon.guedes.sa@gmail.com>
2023-01-11 09:09:17 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Iremos somar 2 números")
+
+let numberOne = prompt("Insira o primeiro número: ")
+let numberTwo = prompt("Insira o segundo número: ")
+let result = Number(numberOne) + Number(numberTwo)
+
+alert("Resultado da soma é: " + result)
